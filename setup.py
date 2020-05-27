aaa65ef1908af4e58858884d4e8b836cf525a67e
Valentin Alexeev <valexeev@cyp.openwaygroup.com>
2020-05-27 15:00:38 +0300

    build: :wrench: Update to the latest setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='pdunehd',
-	version='1.3.1',
-	description='A Python wrapper for Dune HD media player API',
-	author='Valentin Alexeev',
-	author_email='valentin.alekseev@gmail.com',
-	url='https://github.com/valentinalexeev/pdunehd',
-	packages=['pdunehd'],
-	install_requires=['requests']
+setuptools.setup(
+    name="pdunehd",
+    version="1.3.1",
+    author="Valentin Alexeev",
+    author_email="valentin.alekseev@gmail.com",
+    description="A Python wrapper for Dune HD media player API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/valentinalexeev/pdunehd',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
