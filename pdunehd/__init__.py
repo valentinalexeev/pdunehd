2d4742fa02b9a151c026cb5e8ae1e3bf63c8db43
bieniu <maciej.bieniek@gmail.com>
2020-05-27 09:19:31 +0200

    try..except for requests.get

--- pdunehd/__init__.py
+++ pdunehd/__init__.py
@@ -23,7 +23,7 @@ class DuneHDPlayer():
 
 	def pause(self):
 		return self.__change_playback_speed(PLAYBACK_SPEED_PAUSE)
-	
+
 	def ffwd(self):
 		return self.__change_playback_speed(PLAYBACK_SPEED_FFWD)
 
@@ -78,10 +78,13 @@ class DuneHDPlayer():
 
 	def __send_command(self, cmd, params = {}):
 		params["cmd"] = cmd
-		r = requests.get(
-			BASE_COMMAND_URL_FORMAT.format(self._address),
-			params = params
-			)
+		try:
+			r = requests.get(
+				BASE_COMMAND_URL_FORMAT.format(self._address),
+				params = params
+				)
+		except requests.exceptions.ConnectionError:
+			return {}
 
 		if r.status_code == 200:
 			return self.__parse_status(r.text)
